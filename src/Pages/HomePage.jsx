052f9342d679cfd093f1ff666b018b48118b5d72
Lawrence <honyadjilawrence2s1@gmail.com>
2025-05-09 21:01:39 +0000

    Updates done

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import HeroSection from '../components/Sections/HeroSection';
-import ApproveSection from '../components/Sections/ApproveSection';
-import SkillSection from '../components/Sections/SkillSection';
-import ChooseUsSection from '../components/Sections/ChooseUsSection';
-import MemberSection from '../components/Sections/MemberSection';
+import HeroSection from '../components/Sections/HeroSection'
+import ApproveSection from '../components/Sections/ApproveSection'
+import SkillSection from '../components/Sections/SkillSection'
+import ChooseUsSection from '../components/Sections/ChooseUsSection'
+import MemberSection from '../components/Sections/MemberSection'
 
 const Homepage = () => {
     return (
