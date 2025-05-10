c04468edecffd1156dcb7c1c4b26651b3e192e47
Lawrence <honyadjilawrence2s1@gmail.com>
2025-05-10 02:20:16 +0000

    Fix: Vercel refresh issue

--- src/Pages/RegisterPage.jsx
+++ src/Pages/RegisterPage.jsx
@@ -266,7 +266,7 @@ const RegisterPage = () => {
               <button
                 type="submit"
                 disabled={isSubmitting}
-                className="flex w-full justify-center rounded-md bg-violet-600 px-3 py-2.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-violet-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-600 disabled:opacity-50"
+                className="flex w-full justify-center rounded-md bg-violet-600 px-3 py-2.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-violet-500 focus-visible:outline focus-visible:outline-offset-2 focus-visible:outline-violet-600 disabled:opacity-50"
               >
                 {isSubmitting ? "Submitting..." : "Submit Application"}
               </button>
